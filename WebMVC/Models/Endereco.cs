9d9411467f2415ab66e22e495e9e7cf435f8ba0c
andersonxgama <andersonxgama@gmail.com>
2026-01-09 17:34:51 -0300

    Estruturacao WebMVC

--- WebMVC/Models/Endereco.cs
+++ WebMVC/Models/Endereco.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -6,7 +7,18 @@ using System.Text;
 
 namespace WebMVC.Models
 {
-    internal class Endereco
+    public class Endereco
     {
+        public int ClienteId { get; set; }
+        public string CEP { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+
+        [JsonIgnore]
+        public Cliente Cliente { get; set; }
     }
 }
---
+++ WebMVC/Services/ClientesApiService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using WebMVC.Models;
+
+namespace WebMVC.Services
+{
+    public class ClientesApiService
+    {
+        private readonly string baseUrl = "http://localhost:51456/api/clientes";
+
+        public async Task<List<Cliente>> GetClientesAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("");
+                if (response.IsSuccessStatusCode)
+                {
+                    var clientes = await response.Content.ReadAsAsync<List<Cliente>>();
+                    return clientes;
+                }
+                else
+                {
+                    throw new Exception("Erro ao acessar a API: " + response.StatusCode);
+                }
+            }
+        }
+    }
+}
